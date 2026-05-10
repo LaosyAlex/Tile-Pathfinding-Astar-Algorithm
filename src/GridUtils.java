a363a22da4ca73657db6ae0911fa1faf1b141f1d
Alex <Phommarathalex2@gmail.com>
2026-05-10 11:33:54 -0700

    feat(grid): add grid printing as a numbered grid

--- src/GridUtils.java
+++ src/GridUtils.java
@@ -40,4 +40,21 @@ public class GridUtils {
 
         return dim; 
     }
+    public static void printGrid(char[][] grid){
+        System.out.print("+");
+
+        for (int i = 0; i <= grid.length; i++){
+            System.out.print(i);
+        }
+
+        System.out.print("\n");
+
+        for (int i = 0; i < grid.length; i++){
+            System.out.print(i);
+            for (int j = 0; j < grid[0].length; j++){
+                System.out.print(grid[i][j]);
+            }
+            System.out.print("\n");
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,7 @@ public class Main {
         int[] dim = GridUtils.sizeSpecificier();
 
         char[][] grid = GridUtils.generateGrid(dim[0], dim[1]);
-        for (int i = 0; i < dim[0]; i++){
-            for (int j = 0; j < dim[1]; j++){
-                System.out.print(grid[i][j]);
-            }
-            System.out.print("\n");
-        }
+
+        GridUtils.printGrid(grid);
     }
 }
