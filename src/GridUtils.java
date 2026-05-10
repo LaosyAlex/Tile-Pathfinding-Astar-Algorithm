8e7e11505991dc262eaddfc9c40ac7a562b15004
Alex <Phommarathalex2@gmail.com>
2026-05-10 16:02:48 -0700

    style(grid): properly spaced out the numbers for readability and adaptability to digits >1

--- src/GridUtils.java
+++ src/GridUtils.java
@@ -41,20 +41,32 @@ public class GridUtils {
         return dim; 
     }
     public static void printGrid(char[][] grid){
+        int slotSize = String.valueOf(Math.abs(grid[0].length - 1)).length() + 1;
+
         System.out.print("+");
+        System.out.print(" ".repeat(slotSize - 1));
 
         for (int i = 0; i < grid.length; i++){
             System.out.print(i);
+            System.out.print(" ".repeat(slotSize - String.valueOf(Math.abs(i)).length()));
         }
 
         System.out.print("\n");
 
         for (int i = 0; i < grid.length; i++){
             System.out.print(i);
+            System.out.print(" ".repeat(slotSize - String.valueOf(Math.abs(i)).length()));
             for (int j = 0; j < grid[0].length; j++){
                 System.out.print(grid[i][j]);
+                System.out.print(" ".repeat(slotSize - 1)); //Math.abs() turns the $,% etc into ASCII code (usually 2 digits despite 1 symbol) 
             }
             System.out.print("\n");
         }
     }
 }
+
+/*
++  0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+1  a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z
+2  z  y  x  w  v  u  t  s  r  q  p  o  n  m  l  k  j  i  h  g  f  e  d  c  b  a
+*/
